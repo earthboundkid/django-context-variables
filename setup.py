17196792cc991d8a0abf8a042defd040aaad2223
Carl Johnson <cmjohnson@pbs.org>
2015-09-22 22:40:36 -0400

    Fix setup files
    This is a mandatory commit for all Python projects: the one where you
    randomly change your setup.py related files in hopes that it will work.

--- setup.py
+++ setup.py
@@ -6,12 +6,23 @@ def read(fname):
 
 setup(
     name='django-context-variables',
-    version='0.0.2',
+    version='0.0.3',
     author='Carl M. Johnson',
     packages=['context_variables'],
     url='https://github.com/carlmjohnson/django-context-variables',
     license='MIT',
-    description='Simple utility to help make declarative class-based views in Django',
+    description="Simple utility to help make declarative class-based views in Django",
     long_description=read('README.md'),
-    data_files=[('', ['LICENSE', 'README.md'])]
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+    ],
 )
+
